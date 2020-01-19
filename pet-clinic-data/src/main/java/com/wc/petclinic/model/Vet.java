785dba8e9def51c9220f96eb92edee7a28de8bb7
Lenovo <dddfdf>
2020-01-19 19:58:41 +0630

    multi-maven-install

---
+++ pet-clinic-data/src/main/java/com/wc/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.wc.petclinic.model;
+
+public class Vet extends Person {
+
+}
---
+++ pet-clinic-data/src/test/java/com/wc/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wc.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PetclinicApplicationTests.class)
+class PetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
